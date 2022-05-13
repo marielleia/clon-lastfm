2cc532079e417c09e5362311661837146a8c697e
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-13 15:21:26 +0200

    track list en html

--- script.js
+++ script.js
@@ -2,30 +2,21 @@
 const tracksContainer = document.querySelector('.tracks-container');
 
 /* Fetch: Llamamos al json con el listado de canciones */
+const tabla = document.querySelector('.trackstable'); 
+    function traerMusica() { 
 fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
   .then(response => response.json())
-    .then ((music) => { 
-        let listaDeMusica = music.map((music) => {
-            return {artist: music.artist.name, 
-                    name: music.name, 
-                    listeners: music.listeners
-                    }
-                });
+  .then ((music) => { music.map((music) => { 
+      const row = document.createElement('tr'); 
+      row.innerHTML += 
+      ` <td>${music.artist.name}</td> 
+        <td>${music.name}</td> 
+        <td>${music.listeners}</td>`;
+        tabla.appendChild(row); 
+    });
+ }) 
+} 
+traerMusica();
 
                 let showData = JSON.stringify(listaDeMusica);
        document.querySelector(".tracks-container").innerHTML = showData;
-    })
-
-//PLANTILLA CAJA LISTAS MUSICA//
-/*function musicTemplate (music){
-    const card = document.createElement("div");
-    card.classList.add("music-card");
-
-//nombre cancion
-const name = document.createElement("p");
-name.classList.add("name");
-name.textContent = music.name.charAt(0).toUpperCase() + music.name.slice(1);
-
-card.appendChild(name);
-tracksContainer.appendChild(card);
-}*/
