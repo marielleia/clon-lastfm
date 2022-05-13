88901dc0bed96c1acbee5067c85f06020e75b531
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-13 08:42:10 +0200

    stringify

--- script.js
+++ script.js
@@ -2,61 +2,30 @@
 const tracksContainer = document.querySelector('.tracks-container');
 
 /* Fetch: Llamamos al json con el listado de canciones */
-fetch('music.json')
+fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
   .then(response => response.json())
-  .then(data => console.log(data))
+    .then ((music) => { 
+        let listaDeMusica = music.map((music) => {
+            return {artist: music.artist.name, 
+                    name: music.name, 
+                    listeners: music.listeners
+                    }
+                });
 
+                let showData = JSON.stringify(listaDeMusica);
+       document.querySelector(".tracks-container").innerHTML = showData;
+    })
 
-/*fetchManyTracks: Nos permite llamar a la X número de veces para ver diferentes tracks 
-function fetchManyTracks(number) {
-    for (let i = 0; i <= number; i++) {
-        fetchOneTrack(i);
-    }}
+//PLANTILLA CAJA LISTAS MUSICA//
+/*function musicTemplate (music){
+    const card = document.createElement("div");
+    card.classList.add("music-card");
 
-console.log(fetchOneTrack(1));
-
-//-------------------------create music list-----------------------//
-
-function createTrackLine(track) {
-
-//---------------------create line/div --------------------------//
-const card = document.createElement("div");
-card.classList.add("track-card");
-
-// llamar cancion//
-
-//imagen-container//
-const spriteContainer = document.createElement("div");
-spriteContainer.classList.add("img-container");
-
-//-----------------------imagen icono play-------------------------//
-const sprite = document.createElement("img");
-sprite.src = track.sprites.front_default;
-    
-//-------------------------nombre artista-------------------------//
-const artist = document.createElement("p");
-artist.classList.add("artist-name");
-artist.textContent = artist.charAt(0).toUpperCase() + artist.slice(1);
-
-//-------------------------nombre cancion-------------------------//
+//nombre cancion
 const name = document.createElement("p");
-name.classList.add("song-name");
-name.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+name.classList.add("name");
+name.textContent = music.name.charAt(0).toUpperCase() + music.name.slice(1);
 
-//------------------------link a más info--------------------------//
-const aTag = document.createElement ('a');
-aTag.classList.add('link');
-aTag.setAttribute('href', 'music.html');
-aTag.textContent = "More info";
-    
-//Añadir elementos a la card//
-spriteContainer.appendChild(sprite);
-card.appendChild(spriteContainer);
-card.appendChild(artist);
 card.appendChild(name);
-card.appendChild(aTag);
-    
 tracksContainer.appendChild(card);
-
-fetchManyTracks(5);
 }*/
