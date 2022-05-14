c4342e5d5279a67628290753b74c900750636aba
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-14 11:01:37 +0200

    evento click

--- script.js
+++ script.js
@@ -7,50 +7,71 @@ const tabla = document.querySelector('.trackstable');
     function traerMusica() { 
 fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
   .then(response => response.json())
-  .then ((music) => { music.map((music) => { 
+  .then ((music) => {music.map((music) => { 
       const row = document.createElement('tr'); 
       row.innerHTML += 
-
       ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
         <td>${music.artist.name}</td> 
         <td><b>${music.name}</b></td> 
         <td>${music.listeners} listeners</td>`;
         tabla.appendChild(row); 
     });
- }) 
-} 
-traerMusica();
-
-                let showData = JSON.stringify(listaDeMusica);
-       document.querySelector(".tracks-container").innerHTML = showData;
+ })
+let showData = JSON.stringify(listaMusica);
+document.querySelector(".tracks-container").innerHTML = showData;
+traerMusica();}
+                
 
 //PRUEBAS, NO FUNCIONAN TODAVIA//
 //-----Filtrar las canciones más escuchadas del momento------------------//
 
-let btnTop10 = document.getElementById("btn-top10");
-
-function getMinMax (arr){
-    let maximum = Math.max(arr);
-    let minimum = Math.min(arr);
-    let result = ([maximum, minimum]);
-    return result;
-};
 
 
-
-
-
-
+//evento click en botones//
 /*
-function findTop10 (music){
-let top10 = findTop10.sort(function (a,b) {return b-a;}).slice(0,10);
-console.log(findTop10);}
+let btnTop10 = document.querySelector("#btn-top10");
+let btnOverview = document.querySelector("#btn-overview");
+let btnBiggest = document.querySelector("#btn-biggest");
+let listaOverview = document.querySelector(".lista-overview");
+let listaTop10 = document.querySelector(".lista-top10");
+let listaBiggest = document.querySelector(".lista-biggest")
+let subtitulo = document.querySelector(".subtitulo");
 
-btnTop10.onclick = () => {
-    document.querySelector("top10list").classList="btn-top10"
+btnOverview.onclick = ()=> {
+    listaOverview.classList=("overview");
+    document.querySelector(".tracks-container").classList = "overviewTable"
+    document.querySelector(".tracks-container2").classList.remove = "top10Table";
+    document.querySelector(".tracks-container3").classList.remove = "biggestTable";
 }
 
-///H7-------------------filtro de musica jazz----------------------------//
+btnTop10.onclick = ()=> {
+    listaTop10.classList=("top10");
+    document.querySelector(".tracks-container2").classList= "top10Table";
+    document.querySelector(".tracks-container").classList.remove = "overviewTable";
+    document.querySelector(".tracks-container3").classList.remove = "biggestTable";
+}
+
+btnBiggest.onclick  = ()=> {
+    listaBiggest.classList=("biggest");
+    document.querySelector(".tracks-container3").classList = "biggestTable";
+    document.querySelector(".tracks-container2").classList.remove = "top10Table"
+    document.querySelector(".tracks-container").classList.remove = "overviewTable";
+}
+*/
+    /*btnTop10.addEventListener("click", showTop10);
+        document.querySelector(".subtitulo").innerHTML = "Top 10 Listened";
+        function showTop10 (){
+            fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
+            .then(response => response.json())
+            .then (function (music) {
+                let top10 = "";
+                let showTop10 = music.sort((a,b) => a-b).slice(0,10); //sort sirve para filtrar
+                document.querySelector(".subtitulo").innerHTML = top10;
+                        shopTop10();}
+                )}*/
+    
+
+    ///H7-------------------filtro de musica jazz----------------------------//
 /*let jazz = document.querySelector("#jazz")
 jazz.addEventListener("click", listaJazz);
 
