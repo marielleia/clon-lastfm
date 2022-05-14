ed86e35e6f8bbe2d5c13c76894572777382b0a24
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-14 11:53:52 +0200

    add links to artist songs

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-/* El div tracks-container es el único div que existe en el HTML */
-const tracksContainer = document.querySelector('.tracks-container');
+//Llamamos a los elementos del HTML//
+let btnTop10 = document.querySelector("#btn-top10");
+let btnOverview = document.querySelector("#btn-overview");
+let btnBiggest = document.querySelector("#btn-biggest");
+
+let listaOverview = document.querySelector(".lista-overview");
+let listaTop10 = document.querySelector(".lista-top10");
+let listaBiggest = document.querySelector(".lista-biggest");
+
+let tablaOverview = document.querySelector('.tracksTable1');
+let tablaTop10 = document.querySelector('.tracksTable2'); 
+let tablaBiggest = document.querySelector('.tracksTable3');
+
+let h2Overview = document.querySelector("#subtituloO");
+let h2Top10 = document.querySelector("#subtitulo10");
+let h2OBiggest = document.querySelector("#subtituloB");
+
 window.addEventListener("load",traerMusica);
 
 // Aquí llamamos al json con el listado de canciones y lo mostramos en una tabla
-const tabla = document.querySelector('.trackstable'); 
+
     function traerMusica() { 
 fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
   .then(response => response.json())
@@ -11,64 +26,58 @@ fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
       const row = document.createElement('tr'); 
       row.innerHTML += 
       ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
-        <td>${music.artist.name}</td> 
-        <td><b>${music.name}</b></td> 
+        <td><a href=${music.artist.url}>${music.artist.name}</a></td> 
+        <td><a href=${music.url}><b>${music.name}</b></a></td> 
         <td>${music.listeners} listeners</td>`;
-        tabla.appendChild(row); 
+        tablaOverview.appendChild(row); 
     });
  })
 let showData = JSON.stringify(listaMusica);
-document.querySelector(".tracks-container").innerHTML = showData;
+document.querySelector(".lista-overview").innerHTML = showData;
 traerMusica();}
                 
+
 
 //PRUEBAS, NO FUNCIONAN TODAVIA//
 //-----Filtrar las canciones más escuchadas del momento------------------//
 
 
-
 //evento click en botones//
-/*
-let btnTop10 = document.querySelector("#btn-top10");
-let btnOverview = document.querySelector("#btn-overview");
-let btnBiggest = document.querySelector("#btn-biggest");
-let listaOverview = document.querySelector(".lista-overview");
-let listaTop10 = document.querySelector(".lista-top10");
-let listaBiggest = document.querySelector(".lista-biggest")
-let subtitulo = document.querySelector(".subtitulo");
 
+
+/*
 btnOverview.onclick = ()=> {
     listaOverview.classList=("overview");
-    document.querySelector(".tracks-container").classList = "overviewTable"
-    document.querySelector(".tracks-container2").classList.remove = "top10Table";
-    document.querySelector(".tracks-container3").classList.remove = "biggestTable";
+    document.querySelector(".lista-overview").classList = "overviewTable"
+    document.querySelector(".lista-top10").classList.remove = "top10Table";
+    document.querySelector(".lista-biggest").classList.remove = "biggestTable";
 }
 
 btnTop10.onclick = ()=> {
     listaTop10.classList=("top10");
-    document.querySelector(".tracks-container2").classList= "top10Table";
-    document.querySelector(".tracks-container").classList.remove = "overviewTable";
-    document.querySelector(".tracks-container3").classList.remove = "biggestTable";
+    document.querySelector(".lista-top10").classList= "top10Table";
+    document.querySelector(".lista-overview").classList.remove = "overviewTable";
+    document.querySelector(".lista-biggest").classList.remove = "biggestTable";
 }
 
 btnBiggest.onclick  = ()=> {
     listaBiggest.classList=("biggest");
-    document.querySelector(".tracks-container3").classList = "biggestTable";
-    document.querySelector(".tracks-container2").classList.remove = "top10Table"
-    document.querySelector(".tracks-container").classList.remove = "overviewTable";
+    document.querySelector(".lista-biggest").classList = "biggestTable";
+    document.querySelector(".lista-top10").classList.remove = "top10Table"
+    document.querySelector(".lista-overview").classList.remove = "overviewTable";
 }
-*/
-    /*btnTop10.addEventListener("click", showTop10);
+
+btnTop10.addEventListener("click", showTop10);
         document.querySelector(".subtitulo").innerHTML = "Top 10 Listened";
         function showTop10 (){
             fetch('http://127.0.0.1:5500/clon-lastfm/music.json')
             .then(response => response.json())
             .then (function (music) {
                 let top10 = "";
-                let showTop10 = music.sort((a,b) => a-b).slice(0,10); //sort sirve para filtrar
-                document.querySelector(".subtitulo").innerHTML = top10;
-                        shopTop10();}
-                )}*/
+                let showTop10 = music.listeners.sort((a,b) => a-b).slice(0,10); //sort sirve para filtrar
+                document.querySelector(".trackstable").innerHTML = top10;
+                        shopListaTop10();}
+                )}
     
 
     ///H7-------------------filtro de musica jazz----------------------------//
