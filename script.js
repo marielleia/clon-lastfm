78e7f055387583b82b365f97aa4f3547d543f59f
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-16 20:06:18 +0200

    top10 listeners

--- script.js
+++ script.js
@@ -15,58 +15,72 @@ let h2Overview = document.querySelector("#subtituloO");
 let h2Top10 = document.querySelector("#subtitulo10");
 let h2OBiggest = document.querySelector("#subtituloB");
 
-// Aquí llamamos al json con el listado de canciones y lo mostramos en una tabla
+let music = [];
 
-    function traerMusica() {
+// Aquí llamamos al json con el listado de canciones y lo mostramos en una tabla
+//overview //
+function traerMusica() {
 fetch('music.json')
   .then(response => response.json())
-  .then ((music) => {
-      music.sort((a,b) => b.listeners-a.listeners);
+  
+  .then ((result) => {
+      music = result.sort((a,b) => b.listeners-a.listeners);  
+      console.log("we enter the fetch music func")
+      console.log(music)
       music.map((music) => { 
-      const row = document.createElement('tr'); 
-      row.innerHTML += 
-      ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
-        <td><a href=${music.artist.url}>${music.artist.name}</a></td>
-        <td><a href=${music.url}><b>${music.name}</b></a></td>
-        <td>${music.listeners} listeners</td>`;
+        const row = document.createElement('tr'); 
+        row.innerHTML += 
+            ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
+            <td><a href=${music.artist.url}>${music.artist.name}</a></td>
+            <td><a href=${music.url}><b>${music.name}</b></a></td>
+            <td>${music.listeners} listeners</td>`;
         tablaOverview.appendChild(row); 
     });
  })};
 
 //PRUEBAS, NO FUNCIONAN TODAVIA//
 //-----Filtrar las canciones más escuchadas del momento-----//
-const top10 = (traerMusica);
-function filtrarTop10(top10) {
-//fetch('music.json')
-//.then(response => response.json())
-    return (top10.sort ((a,b) => b.listeners-a.listeners))};
+//const top10 = traerMusica();
+//function filtrarTop10(music) {
+//    return (music.sort ((a,b) => b.listeners-a.listeners).slice(0,9))};
 //    console.log(top10);
-document.querySelector('.tracksTable2').innerHTML = top10;      
+//document.querySelector('.tracksTable2').innerHTML = "Hi!";      
 
-setTimeout (top10, 3000);
-
-window.addEventListener("load",traerMusica);
 //evento click en botones//
 
 /*
-    btnOverview.onclick = ()=> {
-        listaOverview.classList=("overview");
+btnOverview.onclick = ()=> {
+    listaOverview.classList=("overview");
         document.querySelector(".lista-overview").classList = "overviewTable"
         document.querySelector(".lista-top10").classList.remove = "top10Table";
         document.querySelector(".lista-biggest").classList.remove = "biggestTable";
     }
     
-    btnTop10.onclick = ()=> {
+    */
+   btnTop10.onclick = ()=> {
         listaTop10.classList=("top10");
-        document.querySelector(".lista-top10").classList= "top10Table";
+        music.map((music) => { 
+            const row = document.createElement('tr'); 
+            row.innerHTML += 
+                ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
+                <td><a href=${music.artist.url}>${music.artist.name}</a></td>
+                <td><a href=${music.url}><b>${music.name}</b></a></td>
+                <td>${music.listeners} listeners</td>`;
+            tablaTop10.appendChild(row); 
+        });}
+
+
+        /*document.querySelector(".lista-top10").classList= "top10Table";
         document.querySelector(".lista-overview").classList.remove = "overviewTable";
         document.querySelector(".lista-biggest").classList.remove = "biggestTable";
     }
     
     btnBiggest.onclick  = ()=> {
-        listaBiggest.classList=("biggest");
+        listaBigge
+        st.classList=("biggest");
         document.querySelector(".lista-biggest").classList = "biggestTable";
         document.querySelector(".lista-top10").classList.remove = "top10Table"
         document.querySelector(".lista-overview").classList.remove = "overviewTable";
     }
-*/
+    */
+    window.addEventListener("load",traerMusica);
