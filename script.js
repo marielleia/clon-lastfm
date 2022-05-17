55b5ee2a67517bfe173f4f68999eba3d9580c8e9
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-17 10:26:45 +0200

    elementos js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 //Llamamos a los elementos del HTML//
-let btnTop10 = document.querySelector("#btn-top10");
-let btnOverview = document.querySelector("#btn-overview");
-let btnBiggest = document.querySelector("#btn-biggest");
-
-let listaOverview = document.querySelector(".lista-overview");
-let listaTop10 = document.querySelector(".lista-top10");
-let listaBiggest = document.querySelector(".lista-biggest");
-
-let tablaOverview = document.querySelector('.tracksTable1');
-let tablaTop10 = document.querySelector('.tracksTable2'); 
-let tablaBiggest = document.querySelector('.tracksTable3');
-
-let h2Overview = document.querySelector("#subtituloO");
-let h2Top10 = document.querySelector("#subtitulo10");
-let h2OBiggest = document.querySelector("#subtituloB");
 
 let genres = document.querySelector(".generos");
 let tracks = document.querySelector(".tracks");
@@ -38,8 +23,8 @@ function traerMusica() {
             <td><a href=${music.artist.url}>${music.artist.name}</a></td>
             <td><a href=${music.url}><b>${music.name}</b></a></td>
             <td>${music.listeners} listeners</td>`;
-        tablaOverview.appendChild(row);
-//aqui hacemos que los elementos/secciones no se vean cuando cargamos la página//
+            tablaOverview.appendChild(row);
+            //aqui hacemos que los elementos/secciones no se vean cuando cargamos la página//
         listaTop10.classList=("invisible");
         listaBiggest.classList=("invisible");
         genres.classList=("invisible");
@@ -49,6 +34,12 @@ function traerMusica() {
  
  //-----------------------OVERVIEW-----------------------------------//
  //Aqui hacemos que al clicar el boton Overview solamente sea visible la lista Overview//
+ 
+ let btnOverview = document.querySelector(".btn-overview");
+ let listaOverview = document.querySelector(".lista-overview");
+ let tablaOverview = document.querySelector('.tracksTable1');
+ let h2Overview = document.querySelector(".subtituloO");
+
  btnOverview.onclick = ()=> {
      listaOverview.classList=("visible");
      listaTop10.classList=("invisible");
@@ -56,26 +47,36 @@ function traerMusica() {
      genres.classList=("invisible");
     }
 //--------------------TOP 10 LISTENERS---------------------------//
- //Aqui hacemos que al clicar el boton Top 10 Listeners solamente sea visible la lista top 10 y se oculten las demás secciones.//
+//Aqui hacemos que al clicar el boton Top 10 Listeners solamente sea visible la lista top 10 y se oculten las demás secciones.//
+let btnTop10 = document.querySelector(".btn-top10");
+let listaTop10 = document.querySelector(".lista-top10");
+let tablaTop10 = document.querySelector('.tracksTable2'); 
+let h2Top10 = document.querySelector(".subtitulo10");
+
 btnTop10.onclick = ()=> {
-        music
-        .slice(0,9)
-        .map((music) => { 
-            const row = document.createElement('tr'); 
-            row.innerHTML += 
-                ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
-                <td><a href=${music.artist.url}>${music.artist.name}</a></td>
-                <td><a href=${music.url}><b>${music.name}</b></a></td>
-                <td>${music.listeners} listeners</td>`;
-            tablaTop10.appendChild(row);
-        listaTop10.classList=("visible");
-        listaOverview.classList=("invisible");
-        listaBiggest.classList=("invisible");
-        genres.classList=("invisible");
-        });}
+    music
+    .slice(0,9)
+    .map((music) => { 
+        const row = document.createElement('tr'); 
+        row.innerHTML += 
+            ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
+            <td><a href=${music.artist.url}>${music.artist.name}</a></td>
+            <td><a href=${music.url}><b>${music.name}</b></a></td>
+            <td>${music.listeners} listeners</td>`;
+        tablaTop10.appendChild(row);
+    listaTop10.classList=("visible");
+    listaOverview.classList=("invisible");
+    listaBiggest.classList=("invisible");
+    genres.classList=("invisible");
+});}
 
 //------------------THE BIGGEST-------------------------------//
  //Aqui hacemos que al clicar el boton The biggest solamente sea visible la lista The biggest y se oculten las demás secciones.//
+
+ let btnBiggest = document.querySelector(".btn-biggest");
+ let listaBiggest = document.querySelector(".lista-biggest");
+ let tablaBiggest = document.querySelector('.tracksTable3');
+ let h2OBiggest = document.querySelector(".subtituloB");
 
 btnBiggest.onclick  = ()=> {
         music
@@ -98,23 +99,22 @@ btnBiggest.onclick  = ()=> {
 //--------------------ROCK -------------------------------//
  //Aqui hacemos que al clicar el boton Rock solamente sea visible la lista Rock y se oculten las demás secciones.//
 
-let rock = document.querySelector("#rock");
-let botonRock = document.querySelector("#title_rock");
+let rock = document.querySelector(".rock");
+let botonRock = document.querySelector(".title_rock");
 let listaRock = document.querySelector(".lista-rock");
 let tablaRock = document.querySelector(".tracksTable4");
 
 botonRock.onclick  = ()=> {
     music
-    .filter((music)=>{music.genres === "rock"})
-    console.log("Hola,esto funciona?")
+    .filter(music=>music.genres === "rock")
     .map((music) => { 
         const row = document.createElement('tr'); 
         row.innerHTML += 
-        ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
-        <td><a href=${music.artist.url}>${music.artist.name}</a></td>
-        <td><a href=${music.url}><b>${music.name}</b></a></td>
-        <td>${music.listeners} listeners</td>`;
-        tablaRock.appendChild(row);
+            ` <td><img id="icon" src="https://cdn-icons-png.flaticon.com/512/5018/5018505.png"></td>
+            <td><a href=${music.artist.url}>${music.artist.name}</a></td>
+            <td><a href=${music.url}><b>${music.name}</b></a></td>
+            <td>${music.listeners} listeners</td>`;
+        tablaRock.appendChild(row); 
         listaRock.classList=("visible");
         tracks.classList=("invisible");
     })}
