d867a49d344b352d0e5da7b3ae3722ab3231192b
Marielle <marielle.ibias@example.comgit config --global user.name Mariellegit config --global user.email marielle.ibias@example.com>
2022-05-19 10:28:11 +0200

    npm

--- script.test.js
+++ script.test.js
@@ -1,5 +1,10 @@
-import {probandoTest} from './script.js';
+import {probandoTest} from './script.js'
 
+test ("1+1=2", () => {
+    expect (1+1).toBe(2);
+})
+
+/*
 describe ("Probar funciones de Javascript"), () => {
     test ("Las funciones pueden tener 1 parámetro"), () => {
         let message = "sale la lista de musica?"
@@ -8,4 +13,4 @@ describe ("Probar funciones de Javascript"), () => {
         return `Hola, ${message}`;
     }
     expect (probandoTest(message)).toBe("Hola, sale la lista de musica?");
-}}
+}}*/
